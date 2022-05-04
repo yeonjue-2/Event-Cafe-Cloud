ad7f8c8bf34cde9aaae036ad6403e8d36a01e890
nnakki <yvely225@gmail.com>
2022-05-04 14:40:52 +0900

    feat / 기본 폴더 구조 생성
    feat / 기본 폴더 구조 생성

---
+++ application.py
@@ -0,0 +1,25 @@
+from pymongo import MongoClient
+import jwt
+import datetime
+import hashlib
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from datetime import datetime, timedelta
+
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
+
+SECRET_KEY = 'SPARTA'
+
+client = MongoClient('내AWS아이피', 27017, username="아이디", password="비밀번호")
+db = client.dbsparta_plus_week4
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
