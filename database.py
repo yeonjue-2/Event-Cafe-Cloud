4a990a0b684c55ee7c11b6b86a10bd6ccbf43102
nnakki <yvely225@gmail.com>
2022-05-08 16:03:40 +0900

    [feat] 토큰 클래스 추가
    feat : ectoken.py
           get_token() : 로그인 유저 정보 가져오기
           get_user_id() : 로그인 유저 아이디 가져오기
    
    fix : databse.py
         리스트 불러오기 함수, db 자동 증가 번호를 위한 함수 추가

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import pymongo
 
-class DB(object):
 
+class DB(object):
     URI = "mongodb://localhost:27017"
 
     @staticmethod
@@ -13,7 +13,6 @@ class DB(object):
     def insert(collection, data):
         DB.DATABASE[collection].insert_one(data)
 
-
     @staticmethod
     def find_one(collection, query):
         return DB.DATABASE[collection].find_one(query)
@@ -22,5 +21,14 @@ class DB(object):
     def update_one(collection, query, update):
         return DB.DATABASE[collection].update_one(query, update)
 
+    @staticmethod
+    def list(collection, query1, query2):
+        return list(DB.DATABASE[collection].find(query1, query2))
 
+    @staticmethod
+    def count(collection):
+        return DB.DATABASE[collection].estimated_document_count({})
 
+    @staticmethod
+    def idx_plus(collection):
+        return DB.DATABASE[collection].find_one(sort=[("idx", -1)])['idx'] + 1
