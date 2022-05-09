aadcbc1d596a8bfbbfd9bba40eebc0e7c94d65d5
nnakki <yvely225@gmail.com>
2022-05-09 20:28:45 +0900

    중간 완료

--- controller/main_controller.py
+++ controller/main_controller.py
@@ -12,7 +12,6 @@ def home():
     user = ECTOKEN.get_token(object)
     if user is not None:
         cafes = DB.list('cafes', {}, {'_id': False})
-        print(cafes)
         for cafe in cafes:
             cafe_idx = str(cafe['idx'])
 
@@ -27,6 +26,11 @@ def home():
     else:
         return render_template('index.html', msg="로그인 정보가 없습니다")
 
+@bp.route('/api/main', methods=['GET'])
+def show_cafes():
+    msg = request.args.get('msg')
+    cafes = DB.list('cafes', {}, {'_id': False})
+    return jsonify({'result': 'success', 'cafes': cafes})
 
 @bp.route("/listing", methods=['GET'])
 def listing():
@@ -34,7 +38,7 @@ def listing():
     cafes = DB.list('cafes', {}, {'_id': False})
     # 포스팅 목록 받아오기
     for cafe in cafes:
-        cafe_idx = cafe['idx']
+        cafe_idx = str(cafe['idx'])
 
         cafe["count_heart"] = DB.count_documents('hearts', {"cafe_idx": cafe_idx, "type": "heart"})
         cafe["heart_by_me"] = bool(DB.find_one('hearts', {"cafe_idx": cafe_idx, "type": "heart", "user_id": user_id}))
@@ -45,7 +49,7 @@ def listing():
     return jsonify({"result": "success", "cafes": cafes})
 
 
-@bp.route('/update_heart', methods=['POST'])
+@bp.route('/update_like', methods=['POST'])
 def update_heart():
     user_id = ECTOKEN.get_user_id(object)
     cafe_idx_receive = request.form["cafe_idx_give"]
