d2aa59c639716863f85698978e60126b3e3b7e62
nnakki <yvely225@gmail.com>
2022-05-10 15:18:55 +0900

    [fix] cafe_controller 주석 처리 해제

--- controller/cafe_controller.py
+++ controller/cafe_controller.py
@@ -8,23 +8,22 @@ bp = Blueprint('cafe', __name__)
 def routeCafeDetail():
     return render_template('cafeDetail.html')
 
-#
-# @bp.route('/api/cafe/detail/<cafeId>')
-# def getCafeDetail(cafeId):
-#     cafe = DB.find_one('cafe', {'cafe_id': cafeId}, {'_id': False})
-#     events = DB.list('event', {'cafe_id': cafeId}, {'_id': False})
-#     reviews = []
-#     for event in events:
-#         review = DB.list('review', {'event_id': event['event_id']}, {'_id': False})
-#         reviews.append(review)
-#     response = {
-#         'cafe_id': cafe['cafe_id'],
-#         'cafe_name': cafe['cafe_name'],
-#         'cafe_info': cafe['cafe_info'],
-#         'address': cafe['address'],
-#         'cafe_image': cafe['cafe_image'],
-#         'x': cafe['x'],
-#         'y': cafe['y'],
-#         'reviews': reviews
-#     }
-#     return jsonify(response)
+@bp.route('/api/cafe/detail/<cafeId>')
+def getCafeDetail(cafeId):
+    cafe = DB.find_one('cafe', {'cafe_id': cafeId}, {'_id': False})
+    events = DB.list('event', {'cafe_id': cafeId}, {'_id': False})
+    reviews = []
+    for event in events:
+        review = DB.list('review', {'event_id': event['event_id']}, {'_id': False})
+        reviews.append(review)
+    response = {
+        'cafe_id': cafe['cafe_id'],
+        'cafe_name': cafe['cafe_name'],
+        'cafe_info': cafe['cafe_info'],
+        'address': cafe['address'],
+        'cafe_image': cafe['cafe_image'],
+        'x': cafe['x'],
+        'y': cafe['y'],
+        'reviews': reviews
+    }
+    return jsonify(response)
