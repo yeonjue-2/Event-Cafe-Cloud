d5869326b6bc12c5c558ed41195c579789d3b118
yeonjue-2 <ilov-@naver.com>
2022-05-11 19:34:17 +0900

    [feat] 이벤트 별 페이지 구현 [fix] 즐겨찾기 기능 수정

--- controller/main_controller.py
+++ controller/main_controller.py
@@ -11,42 +11,60 @@ bp = Blueprint('main', __name__)
 def home():
     user = ECTOKEN.get_token(object)
     if user is not None:
-        cafes = DB.list('cafes', {}, {'_id': False})
+        cafes = DB.list('cafe', {}, {'_id': False})
         for cafe in cafes:
             cafe_idx = str(cafe['idx'])
-            cafe["count_heart"] = DB.count_documents('hearts', {"cafe_idx": cafe_idx, "type": "heart"})
-            cafe["heart_by_me"] = bool(DB.find_one('hearts', {"cafe_idx": cafe_idx, "type": "heart", "user_id": user["user_id"]},{'_id': False}))
-            cafe["bookmark_by_me"] = bool(DB.find_one('hearts', {"cafe_idx": cafe_idx, "type": "bookmark", "user_id": user["user_id"]},{'_id': False}))
+            cafe["count_heart"] = DB.count_documents('heart', {"cafe_idx": cafe_idx, "type": "heart"})
+            cafe["heart_by_me"] = bool(DB.find_one('heart', {"cafe_idx": cafe_idx, "type": "heart", "user_id": user["user_id"]},{'_id': False}))
+            cafe["bookmark_by_me"] = bool(DB.find_one('heart', {"cafe_idx": cafe_idx, "type": "bookmark", "user_id": user["user_id"]},{'_id': False}))
         return render_template('index.html', user=user)
     else:
         return render_template('index.html', msg="로그인 정보가 없습니다")
 
 
-@bp.route('/event_cafe')
-def event_cafe():
+@bp.route('/event_cafe/<event_category>')
+def event_cafe(event_category):
     user = ECTOKEN.get_token(object)
-    if user is None:
-        return render_template('event_cafe.html')
+
+    if user is not None:
+        return render_template('eventCafe.html', user=user, event_category=event_category)
     else:
-        return render_template('event_cafe.html', user=user)
+        return render_template('eventCafe.html', msg="로그인 정보가 없습니다")
 
 @bp.route('/listing', methods=['GET'])
 def listing():
     user_id = ECTOKEN.get_user_id(object)
     cafes = DB.list('cafes', {}, {'_id': False})
+
+    for cafe in cafes:
+        cafe_idx = str(cafe['idx'])
+        cafe["count_heart"] = DB.count_documents('hearts', {"cafe_idx": cafe_idx, "type": "heart"})
+        cafe["heart_by_me"] = bool(
+            DB.find_one('hearts', {"cafe_idx": cafe_idx, "type": "heart", "user_id": user_id}, {'_id': False}))
+        cafe["bookmark_by_me"] = bool(
+            DB.find_one('hearts', {"cafe_idx": cafe_idx, "type": "bookmark", "user_id": user_id}, {'_id': False}))
+
+    return jsonify({"result": "success", "cafes": cafes})
+
+@bp.route('/event_cafe/listing_event', methods=['GET'])
+def listing_event():
+    user_id = ECTOKEN.get_user_id(object)
     event_category_receive = request.args.get("event_category_give")
-    if event_category_receive == "":
-        events = DB.list('events', {}, {'_id': False})
-    else:
-        events = DB.list('events', {'event_category': event_category_receive}, {'_id': False})
+    events = DB.list('events', {"event_category": event_category_receive}, {'_id': False})
+    cafes = []
+
+    for event in events:
+        cafe_id = int(event['cafe_id'])
+        cafe = DB.find_one('cafes', {"idx": cafe_id}, {'_id': False})
+        cafes.append(cafe)
 
     for cafe in cafes:
         cafe_idx = str(cafe['idx'])
         cafe["count_heart"] = DB.count_documents('hearts', {"cafe_idx": cafe_idx, "type": "heart"})
         cafe["heart_by_me"] = bool(DB.find_one('hearts', {"cafe_idx": cafe_idx, "type": "heart", "user_id": user_id}, {'_id': False}))
         cafe["bookmark_by_me"] = bool(DB.find_one('hearts', {"cafe_idx": cafe_idx, "type": "bookmark", "user_id": user_id}, {'_id': False}))
-    return jsonify({"result": "success", 'cafes': cafes, 'events': events})
 
+    return jsonify({"result": "success", "cafes": cafes, "events": events})
 
 @bp.route('/update_heart', methods=['POST'])
 def update_heart():
--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,3 @@
-$(document).ready(function () {
-    listing()
-})
-
 
 function toggle_heart(cafe_idx, type) {
     if ($.cookie("jwt_token") === undefined) {
@@ -58,14 +54,55 @@ function num2str(count) {
 }
 
 function listing() {
-    // todo
-    // if (event_category == undefined) {
-    //     event_category = ""
-    //}
+
     $("#cards-box").empty()
     $.ajax({
         type: "GET",
-        url: "/listing",  // ?evnet_category_give=${event_category}",
+        url: `/listing`,
+        data: {},
+        success: function (response) {
+            let cafes = response['cafes']
+            for (let i = 0; i < cafes.length; i++) {
+                let cafe_idx = cafes[i]['idx']
+                let cafe_image = cafes[i]['cafe_image']
+                let cafe_name = cafes[i]['cafe_name']
+                let cafe_short_info = cafes[i]['cafe_short_info']
+                let class_heart = cafes[i]['heart_by_me'] ? "fa-heart" : "fa-heart-o"
+                let count_heart = cafes[i]['count_heart']
+                let class_bookmark = cafes[i]['bookmark_by_me'] ? "fa-bookmark" : "fa-bookmark-o"
+
+                let temp_html = `<div class="card" id="${cafe_idx}" style="width: 18rem; border-radius: 10px;">
+                                        <img class="card-img-top" src="../static/cafe_pics/${cafe_image}" alt="Card image cap">
+                                        <div class="card-body">
+                                            <h3 class="card-title">${cafe_name}&nbsp;&nbsp;<a class="level-item is-sparta" aria-label="bookmark" onclick="toggle_heart('${cafe_idx}', 'bookmark')" style="color: dodgerblue">
+                                                <span class="icon is-small"><i class="fa ${class_bookmark}" aria-hidden="true"></i></span></a>
+                                            </h3>
+                                            <p class="card-text">${cafe_short_info}</p>
+                                            <nav class="level is-mobile">
+                                                <div class="level-left">
+                                                        <a class="level-item is-sparta" aria-label="heart" style="color:#F5C0BE" onclick="toggle_heart('${cafe_idx}', 'heart')">
+                                                            <span class="icon is-small"><i class="fa ${class_heart}" aria-hidden="true"></i>
+                                                            </span>&nbsp;<span class="heart-num">${num2str(count_heart)}</span>
+                                                        </a>                                                                                                                                                                                                                                                                  
+                                                </div>
+                                            </nav>
+                                            <div class="btn-style">
+                                                <button type="button" class="btn btn-outline-secondary" style="float: right; margin-bottom: 10px;">
+                                                <a href="#" style="color: dimgrey">둘러보기</a></button>
+                                            </div>
+                                 </div>`
+                $('#cards-box').append(temp_html)
+            }
+        }
+    })
+}
+
+function listing_event(event_category) {
+
+    $("#cards-box").empty()
+    $.ajax({
+        type: "GET",
+        url: `/event_cafe/listing_event?event_category_give=${event_category}`,
         data: {},
         success: function (response) {
             let cafes = response['cafes']
