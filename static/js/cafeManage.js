ce26809994ad319e42d97b9ad3f3f63f61411594
nnakki <yvely225@gmail.com>
2022-05-12 22:11:15 +0900

    [fix] datepicker 시작일/종료일 설정 추가 이벤트 등록 시 조건 설정

--- static/js/cafeManage.js
+++ static/js/cafeManage.js
@@ -1,4 +1,4 @@
-$(document).ready(()=>{
+$(document).ready(() => {
     // showCafeManagement();
     makeDatepicker();
 })
@@ -51,21 +51,37 @@ function regCustomDay(idx) {
 }
 
 function makeDatepicker() {
-    $.datepicker.setDefaults({
-        dateFormat: 'yy-mm-dd',
-        prevText: '이전 달',
+    $.datepicker.setDefaults($.datepicker.regional['ko']);
+    $("#startDate").datepicker({
         nextText: '다음 달',
-        monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
-        monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
-        dayNames: ['일', '월', '화', '수', '목', '금', '토'],
-        dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+        prevText: '이전 달',
+        dayNames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
         dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
-        showMonthAfterYear: true,
-        yearSuffix: '년'
+        monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+        monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+        dateFormat: "yy-mm-dd",
+        onClose: function (selectedDate) {
+            $("#endDate").datepicker("option", "minDate", selectedDate);
+        }
+
+    });
+    $("#endDate").datepicker({
+        nextText: '다음 달',
+        prevText: '이전 달',
+        dayNames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
+        dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+        monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+        monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+        dateFormat: "yy-mm-dd",
+        onClose: function (selectedDate) {
+            $("#startDate").datepicker("option", "maxDate", selectedDate);
+        }
+
     });
 
+
     $(function () {
-        $("#datepicker1, #datepicker2").datepicker();
+        $("#startDate, #endDate").datepicker();
     });
 }
 
